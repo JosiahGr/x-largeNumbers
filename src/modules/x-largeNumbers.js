1c70e257e304135eb42758dac52adbdfa5c43baf
Josiah Green <JosiahGreen90@gmail.com>
2018-09-19 10:16:10 -0700

    working on bug with multiply

--- src/modules/x-largeNumbers.js
+++ src/modules/x-largeNumbers.js
@@ -1,9 +1,29 @@
 'use strict';
 
-export default function multiply(a, b) {
+function multiply(a, b) {
   const numA = parseInt(a, 10);
   const numB = parseInt(b, 10);
   const product = numA * numB;
 
   return `${product}`;
+}
+
+function bigNum(int) {
+  if (Math.abs(int) < 1.0) {
+    const e = parseInt(x.toString().split('e-')[1]);
+    let x = int;
+    if (e) {
+      x *= Math.pow(10, e - 1);
+      x = `0.${(new Array(e)).join('0')}${x.toString().substring(2)}`;
+    }
+  } else {
+    let e = parseInt(x.toString().split('+')[1]);
+    let x = int;
+    if (e > 20) {
+      e -= 20;
+      x /= Math.pow(10, e);
+      x += (new Array(e + 1)).join('0');
+    }
+  }
+  return x;
 }
