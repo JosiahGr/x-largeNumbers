3b45175a13e13905e0c436fd1ac8dc6a107435f2
Josiah Green <JosiahGreen90@gmail.com>
2018-09-19 10:51:16 -0700

    large multiplications now working

--- src/modules/x-largeNumbers.js
+++ src/modules/x-largeNumbers.js
@@ -1,29 +1,30 @@
 'use strict';
 
-function multiply(a, b) {
+export default function multiply(a, b) {
   const numA = parseInt(a, 10);
   const numB = parseInt(b, 10);
   const product = numA * numB;
+  
+  function toFixed(num) {
+    let x = num;
+    if (Math.abs(x) < 1.0) {
+      const e = parseInt(x.toString().split('e-')[1], 10);
+      if (e) {
+        x *= 10 ** (e - 1);
+        x = `0.${(new Array(e)).join('0')}${x.toString().substring(2)}`;
+      }
+    } else {
+      let e = parseInt(x.toString().split('+')[1], 10);
+      if (e > 20) {
+        e -= 20;
+        x /= 10 ** e;
+        x += (new Array(e + 1)).join('0');
+      }
+    }
+    return x;
+  }
+
+  toFixed(product);
 
   return `${product}`;
-}
-
-function bigNum(int) {
-  if (Math.abs(int) < 1.0) {
-    const e = parseInt(x.toString().split('e-')[1]);
-    let x = int;
-    if (e) {
-      x *= Math.pow(10, e - 1);
-      x = `0.${(new Array(e)).join('0')}${x.toString().substring(2)}`;
-    }
-  } else {
-    let e = parseInt(x.toString().split('+')[1]);
-    let x = int;
-    if (e > 20) {
-      e -= 20;
-      x /= Math.pow(10, e);
-      x += (new Array(e + 1)).join('0');
-    }
-  }
-  return x;
 }
